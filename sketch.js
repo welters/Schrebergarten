ebe0c9601ffa8b741c0f0c8e58d678625f7c7012
welters <44268427+welters@users.noreply.github.com>
2018-11-12 23:23:56 +0100

    add permutations and bug fixing

--- sketch.js
+++ sketch.js
@@ -1,6 +1,7 @@
 'use strict'
 let gardens, corners, areas, or, sort, copyGardens;
 let sz, ps;
+let displaySize;
 
 function setup() {
 	gardens = [		//manually set gardens
@@ -15,6 +16,7 @@ function setup() {
 	or = [];					//Array of strings for the order the gardens are set
 	copyGardens = [];
 	
+	displaySize = 5;
 	createCanvas(400, 200);
 	document.getElementById('defaultCanvas0').style.visibility = 'visible';
 	background(100);
@@ -23,26 +25,28 @@ function setup() {
 }
 
 function algo() {
+	let p = [0, 1, 2, 3];
 
+	or = perm(p);
 	
 	
-	or = [
-		[0,1,2,3],
-		[1,2,0,3],
-		[1,3,2,0],
-		[3,2,1,0],
-		[2,3,1,0],
-		[3,0,2,1],
-		[0,2,3,1]
-	];
+	// or = [
+	// 	[0,1,2,3],
+	// 	[1,2,0,3],
+	// 	[1,3,2,0],
+	// 	[3,2,1,0],
+	// 	[2,3,1,0],
+	// 	[3,0,2,1],
+	// 	[0,2,3,1]
+	// ];
 
 	for (let o in or) {
 		
 		areas.push([		//manually set gardens
-			{size : createVector(10, 20), pos : undefined, set : false},
-			{size : createVector(15, 25), pos : undefined, set : false},
-			{size : createVector(5, 10), pos : undefined, set : false},
-			{size : createVector(30, 10), pos : undefined, set : false}
+			{size : createVector(10, 15), pos : undefined, set : false},
+			{size : createVector(10, 15), pos : undefined, set : false},
+			{size : createVector(15, 10), pos : undefined, set : false},
+			{size : createVector(15, 10), pos : undefined, set : false}
 		]);
 		corners = undefined;
 		corners = [];
@@ -53,7 +57,7 @@ function algo() {
 		}
 
 		for (let k in areas) {
-		 	if (checkSize(areas[k]) >= checkSize(areas[o])) {
+		 	if ((checkSize(areas[k])[0] *checkSize(areas[k])[1]) >= (checkSize(areas[o])[0] *checkSize(areas[o])[1])) {
 				areas.splice(k, 0, areas[o]);
 				areas.pop();
 		 		break;
@@ -61,31 +65,44 @@ function algo() {
 		}
 
 	}
+	let cons = 0;
 	console.table(areas);
-	display(areas[1]);
-	console.log(checkSize(areas[1]));
+	display(areas[cons]);
+	console.log(checkSize(areas[cons])[0] *checkSize(areas[cons])[1]);
 }
 
 function setElement(g, ind) {
 	let minSize = Infinity;
 	let delCorner = undefined;
-	console.log(g);
 	areas[ind][g].set = true;
 	for (let c in corners) {
 		//console.log(corners, c, corners[c]);
 		areas[ind][g].pos = corners[c];
-		if (checkSize(areas[ind]) < minSize) {
-			minSize = checkSize(areas[ind]);
+		if (checkSize(areas[ind])[0] *checkSize(areas[ind])[1] < minSize  && noOverlap(areas[ind], areas[ind][g])) {
+			minSize = checkSize(areas[ind])[0] *checkSize(areas[ind])[1];
 			delCorner = c;
 		}
 	}
 	areas[ind][g].pos = corners[delCorner];
-	console.log("areas: " + ind + ", garden: " + g + ", posX: " +  areas[ind][g].pos.x + ", posY:" + areas[ind][g].pos.y);
+	//console.log("areas: " + ind + ", garden: " + g + ", posX: " +  areas[ind][g].pos.x + ", posY:" + areas[ind][g].pos.y);
 	corners.splice(delCorner, 1);
 	corners.push(createVector(areas[ind][g].pos.x + areas[ind][g].size.x, areas[ind][g].pos.y));
 	corners.push(createVector(areas[ind][g].pos.x, areas[ind][g].pos.y + areas[ind][g].size.y));
 }
 
+
+function noOverlap(are, g) {
+	for (let a of are) {
+		if (a == g) continue;
+		if (a.pos != undefined) {
+			//console.log("a--  posX: " +  a.pos.x + ", posY:" + a.pos.y + ", sizeX: " + a.size.x + ", sizeY: " + a.size.y);
+			//console.log("g--  posX: " +  g.pos.x + ", posY:" + g.pos.y + ", sizeX: " + g.size.x + ", sizeY: " + g.size.y);
+			if (g.pos.y < (a.pos.y +a.size.y) && (g.pos.x +g.size.x) > a.pos.x && g.pos.x < (a.pos.x +a.size.x)) return(false);
+			if (g.pos.x < (a.pos.x +a.size.x) && (g.pos.y +g.size.y) > a.pos.y && g.pos.y < (a.pos.y +a.size.y)) return(false);
+		}
+	}
+	return(true);
+}
 
 
 function checkSize(a) {
@@ -115,14 +132,36 @@ function checkSize(a) {
 		}
 	}
 	
-	return(r*b);
+	return([r, b]);
+}
+
+function perm(xs) {
+  let ret = [];
+
+  for (let i = 0; i < xs.length; i = i + 1) {
+    let rest = perm(xs.slice(0, i).concat(xs.slice(i + 1)));
+
+    if(!rest.length) {
+      ret.push([xs[i]])
+    } else {
+      for(let j = 0; j < rest.length; j = j + 1) {
+        ret.push([xs[i]].concat(rest[j]))
+      }
+    }
+  }
+  return ret;
 }
 
 function display(ar) {		//displays the area of all gardens
 	background(100);
+	translate(width /2 -(checkSize(ar)[0] /2 *displaySize), height /2 -(checkSize(ar)[1] /2 *displaySize));
+	fill(255, 0, 0);
+	rect(0, 0, checkSize(ar)[0] *displaySize, checkSize(ar)[1] *displaySize)
+	fill(0, 255, 0);
 	for (let g of ar){
 		if (g.set) {
-			rect(g.pos.x, g.pos.y, g.size.x, g.size.y);
+			rect(g.pos.x *displaySize, g.pos.y *displaySize, g.size.x *displaySize, g.size.y *displaySize);
 		}
 	}
+
 }
