457b506ff02543b660a4d4a3b2ec0c1bf6f81c91
welters <44268427+welters@users.noreply.github.com>
2018-11-22 20:50:59 +0100

    working

--- sketch.js
+++ sketch.js
@@ -2,7 +2,6 @@
 let gardens, xVal, maxYVal, areas, or, sort, copyGardens;
 let sz, ps;
 let displaySize;
-let yVal;
 
 let minSize;
 
@@ -17,7 +16,7 @@ function setup() {
 	areas = [];				//Array for areas with all different
 	or = [];					//Array of strings for the order the gardens are set
 	
-	displaySize = 5;
+	displaySize = 10;
 	createCanvas(windowWidth, windowHeight);
 	document.getElementById('defaultCanvas0').style.visibility = 'visible';
 	background(100);
@@ -32,56 +31,63 @@ function algo() {
 
 	for (let o in or) {
 		
-		xVal = [];
-		xVal.push(0);
+		xVal = [0];
+		//xVal.push(0);
 		maxYVal = 0;
-		minSize = Infinity;
+		minSize = Infinity;	
 		areas.push([]);
-		setElement(0, o);		//set position of gardens in given order
-
+		setElement(0, o, [], 0);		//set position of gardens in given order
 	}
-	let cons = 22;
-	//console.table(areas);
-	display(areas[cons]);
-	console.log(checkSize(areas[cons], "size"));
+
+	let cons = 16;
+	console.table(areas);
+	display(cons);
+	//console.log(checkSize(areas[cons], "size"));
 }
 
-function setElement(i, o) {
-	yVal = 0;
+function setElement(i, o, safeArea, maxYVal) {
+	let sA = safeArea;
+	let yVal;
+	let preMaxYVal;
 	let g = or[o][i];
 	for (let x of xVal) {
-		yVal = findYVal(gardens[g], x);
+		yVal = findYVal(gardens[g], x, maxYVal);
+		preMaxYVal = maxYVal;
+		if (maxYVal < yVal + gardens[g].size.y)	maxYVal = yVal + gardens[g].size.y;
 		gardens[g].pos = createVector(x, yVal);
 		gardens[g].set = true;
+		sA.push(gardens[g].pos);
 		xVal.push(gardens[g].pos.x +gardens[g].size.x);
 		if (or[o][i+1] != undefined) {
-			setElement(i+1, o);
+			setElement(i+1, o, sA, maxYVal)
 		} else {
-			if (minSize >= checkSize(gardens, "size")) {
+			if (minSize > checkSize(gardens, "size")) {
 				minSize = checkSize(gardens, "size");
-				areas[o] = gardens.slice();
+				areas[o] = sA.slice();
 			}
 		}
+		sA.pop();
 		xVal.pop();
 		gardens[g].pos = undefined;
 		gardens[g].set = false;
+		//maxYVal = preMaxYVal;
+
 		
 	}
 	
 }
 
-function findYVal(gard, xV) {
-	for (let y = maxYVal; y >= 0; y--) {
+function findYVal(gard, xV, maxYVal) {
+	for (let yV = maxYVal; yV >= 0; yV--) {
 		for (let g of gardens) {
-			if (g.set) {
-				if ((yVal <= g.pos.y +g.size.x) && (
-				(xV < g.pos.x +g.size.x && xV >= g.pos.x)||
-				(xV +gard.size.x <= g.pos.x +g.size.x && xV +gard.size.x > g.pos.x)||
-				(g.pos.x <= xV +gard.size.x && g.pos.x > xV)||
-				(g.pos.x +g.size.x < xV +gard.size.x && g.pos.x +g.size.x >= xV)
-				)) {
-					return(y);
-				}
+			if (!g.set || g == gard) continue;
+			if ((yV <= g.pos.y +g.size.y) && (
+			(xV < g.pos.x +g.size.x && xV >= g.pos.x)||
+			(xV +gard.size.x <= g.pos.x +g.size.x && xV +gard.size.x > g.pos.x)||
+			(g.pos.x < xV +gard.size.x && g.pos.x >= xV)||
+			(g.pos.x +g.size.x <= xV +gard.size.x && g.pos.x +g.size.x > xV)
+			)) {
+				return(yV);
 			}
 		}
 	}
@@ -136,15 +142,21 @@ function perm(xs) {						//algorithm to get all possible orders to set the garde
 }
 
 function display(ar) {		//displays the area of all gardens
+	let gO
+	let gPos = areas[ar];
+	console.log(gPos);
 	background(100);		
-	let tp = createVector(width /2 -(checkSize(ar, "r") /2 *displaySize), height /2 -(checkSize(ar, "b") /2 *displaySize));
-	fill(255, 0, 0);
-	rect(tp.x, tp.y, checkSize(ar, "r") *displaySize, checkSize(ar, "b") *displaySize)
+	//let tp = createVector(width /2 -(checkSize(ar, "r") /2 *displaySize), height /2 -(checkSize(ar, "b") /2 *displaySize));
+	//fill(255, 0, 0);
+	//rect(tp.x, tp.y, checkSize(ar, "r") *displaySize, checkSize(ar, "b") *displaySize)
 	fill(0, 255, 0);
-	for (let g of ar){
-		if (g.set) {
-			rect(tp.x +g.pos.x *displaySize, tp.y +g.pos.y *displaySize, g.size.x *displaySize, g.size.y *displaySize);
-		}
+	for (let gP in gPos){
+		gO = gardens[or[ar][gP]];
+		console.log(gO);
+		rect(gPos[gP].x *displaySize, gPos[gP].y *displaySize, gO.size.x *displaySize, gO.size.y *displaySize);
+		// if (g.set) {
+			//rect(tp.x +g.pos.x *displaySize, tp.y +g.pos.y *displaySize, g.size.x *displaySize, g.size.y *displaySize);
+		// }
 	}
 
 }
